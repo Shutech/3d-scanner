5aab26a8ff147796a4009506add0b3699a266df7
Morten Kals <m.kals@me.com>
2016-10-02 16:31:48 -0700

    removed class and added updated scanning method

--- py_script.py
+++ py_script.py
@@ -1,55 +1,74 @@
+import time
 import subprocess
 import os
 import sys 
 import RPi.GPIO as GPIO 
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+
 ROTATION_COUNT = 3600
 
-class Scanner:
+# Raspberry Pi PGIO ports
+LED_GREEN = 0
+LED_RED = 0
+STEPPER = 0
+SWITCH = 0 
+LASER = 0 
 
-    # Raspberry Pi PGIO ports
-    LED_GREEN = 0
-    LED_RED = 0
-    STEPPER = 0
-    SWITCH = 0 
-    LASER = 0 
+state = State.Ready
 
-    def get_picture(camera):
-        if camera == 'left':
-            subprocess.call(["fswebcam","-r","1920x1080","--no-banner","-d","/dev/video0","/images/image.jpg"])
-        elif camera == 'right':
-            subprocess.call(["fswebcam","-r","1920x1080","--no-banner","-d","/dev/video1","/images/simage.jpg"])
-        f = open("image.jpg","rb")
+class State:
+    Ready, Scanning = range(1,2)
 
-    def rotate_turntable():
-        mh = Adafruit_MotorHAT() 
-        myStepper.setSpeed(30) 
-        myStepper.step(1, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
+def get_picture(camera):
+    if camera == 'left':
+        subprocess.call(["fswebcam","-r","1920x1080","--no-banner","-d","/dev/video0","/images/image.jpg"])
+    elif camera == 'right':
+        subprocess.call(["fswebcam","-r","1920x1080","--no-banner","-d","/dev/video1","/images/simage.jpg"])
+    f = open("image.jpg","rb")
 
-    def laser(state):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(24, GPIO.OUT)
-        if state:
-            GPIO.output(24, 1)
-        else:
-            GPIO.output(24, 0)
+def rotate_turntable():
+    mh = Adafruit_MotorHAT() 
+    myStepper.setSpeed(30) 
+    myStepper.step(1, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
+
+def laser(state):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(24, GPIO.OUT)
+    if state:
+        GPIO.output(24, 1)
+    else:
+        GPIO.output(24, 0)
 
 
-    def main():
+def scanning():
+    for index in range (0,ROTATION_COUNT):
+        
+        laser.off()
+        get_picture('left')
+        get_picture('right')
+        
+        laser.on()
+        get_picture('left')
+        get_gicture('right')
 
-        command = sys.argv[0]
+        rotate_turntable()
+        
 
-        if command == "camera-left":
-            get_picture("left")
-        elif command == "camera-right":
-            get_picture("right")
-        elif command == "turntable":
-            rotate_turntable()
-        elif command == "laser-on":
-            laser(True)
-        elif command == "laser-off":
-            laser(False)
-        print(command)
+def main():
 
-    if __name__ == "__main__":
-        main()
+    command = sys.argv[0]
+
+    if command == "camera-left":
+        get_picture("left")
+    elif command == "camera-right":
+        get_picture("right")
+    elif command == "turntable":
+        rotate_turntable()
+    elif command == "laser-on":
+        laser(True)
+    elif command == "laser-off":
+        laser(False)
+    print(command)
+
+if __name__ == "__main__":
+    main()
